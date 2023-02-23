dab04b55b8e8329cfb7804969fc437f196c78fbe
Borejo <125442451+Borejo@users.noreply.github.com>
2023-02-23 22:48:35 +0100

    Add files via upload

---
+++ Zgadywanka/Zgadywanka/Form1.Designer.cs
@@ -0,0 +1,328 @@
+﻿namespace Zgadywanka
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Ekran = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.button11 = new System.Windows.Forms.Button();
+            this.button12 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.zatwierz_zakres = new System.Windows.Forms.Button();
+            this.zakres_od = new System.Windows.Forms.NumericUpDown();
+            this.zakres_do = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.zakres_od)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.zakres_do)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Ekran
+            // 
+            this.Ekran.Font = new System.Drawing.Font("MV Boli", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Ekran.Location = new System.Drawing.Point(8, 7);
+            this.Ekran.Margin = new System.Windows.Forms.Padding(2);
+            this.Ekran.Name = "Ekran";
+            this.Ekran.Size = new System.Drawing.Size(212, 54);
+            this.Ekran.TabIndex = 0;
+            this.Ekran.Text = "↓Podaj zakres↓";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("MV Boli", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button1.Location = new System.Drawing.Point(15, 110);
+            this.button1.Margin = new System.Windows.Forms.Padding(2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(57, 57);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("MV Boli", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button2.Location = new System.Drawing.Point(15, 183);
+            this.button2.Margin = new System.Windows.Forms.Padding(2);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(57, 57);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "4";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("MV Boli", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button3.Location = new System.Drawing.Point(15, 255);
+            this.button3.Margin = new System.Windows.Forms.Padding(2);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(57, 57);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "7";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("MV Boli", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button4.Location = new System.Drawing.Point(15, 325);
+            this.button4.Margin = new System.Windows.Forms.Padding(2);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(57, 57);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "*";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Font = new System.Drawing.Font("MV Boli", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button5.Location = new System.Drawing.Point(86, 325);
+            this.button5.Margin = new System.Windows.Forms.Padding(2);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(57, 57);
+            this.button5.TabIndex = 8;
+            this.button5.Text = "0";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Font = new System.Drawing.Font("MV Boli", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button6.Location = new System.Drawing.Point(86, 255);
+            this.button6.Margin = new System.Windows.Forms.Padding(2);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(57, 57);
+            this.button6.TabIndex = 7;
+            this.button6.Text = "8";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Font = new System.Drawing.Font("MV Boli", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button7.Location = new System.Drawing.Point(86, 183);
+            this.button7.Margin = new System.Windows.Forms.Padding(2);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(57, 57);
+            this.button7.TabIndex = 6;
+            this.button7.Text = "5";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.Font = new System.Drawing.Font("MV Boli", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button8.Location = new System.Drawing.Point(86, 110);
+            this.button8.Margin = new System.Windows.Forms.Padding(2);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(57, 57);
+            this.button8.TabIndex = 5;
+            this.button8.Text = "2";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button9
+            // 
+            this.button9.Font = new System.Drawing.Font("MV Boli", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button9.Location = new System.Drawing.Point(158, 325);
+            this.button9.Margin = new System.Windows.Forms.Padding(2);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(57, 57);
+            this.button9.TabIndex = 12;
+            this.button9.Text = "#";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // button10
+            // 
+            this.button10.Font = new System.Drawing.Font("MV Boli", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button10.Location = new System.Drawing.Point(158, 255);
+            this.button10.Margin = new System.Windows.Forms.Padding(2);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(57, 57);
+            this.button10.TabIndex = 11;
+            this.button10.Text = "9";
+            this.button10.UseVisualStyleBackColor = true;
+            this.button10.Click += new System.EventHandler(this.button10_Click);
+            // 
+            // button11
+            // 
+            this.button11.Font = new System.Drawing.Font("MV Boli", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button11.Location = new System.Drawing.Point(158, 183);
+            this.button11.Margin = new System.Windows.Forms.Padding(2);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(57, 57);
+            this.button11.TabIndex = 10;
+            this.button11.Text = "6";
+            this.button11.UseVisualStyleBackColor = true;
+            this.button11.Click += new System.EventHandler(this.button11_Click);
+            // 
+            // button12
+            // 
+            this.button12.Font = new System.Drawing.Font("MV Boli", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button12.Location = new System.Drawing.Point(158, 110);
+            this.button12.Margin = new System.Windows.Forms.Padding(2);
+            this.button12.Name = "button12";
+            this.button12.Size = new System.Drawing.Size(57, 57);
+            this.button12.TabIndex = 9;
+            this.button12.Text = "3";
+            this.button12.UseVisualStyleBackColor = true;
+            this.button12.Click += new System.EventHandler(this.button12_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("MV Boli", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(11, 69);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(32, 25);
+            this.label1.TabIndex = 13;
+            this.label1.Text = "od";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("MV Boli", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(101, 70);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(32, 25);
+            this.label2.TabIndex = 15;
+            this.label2.Text = "do";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // zatwierz_zakres
+            // 
+            this.zatwierz_zakres.Font = new System.Drawing.Font("MV Boli", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.zatwierz_zakres.Location = new System.Drawing.Point(198, 67);
+            this.zatwierz_zakres.Margin = new System.Windows.Forms.Padding(2);
+            this.zatwierz_zakres.Name = "zatwierz_zakres";
+            this.zatwierz_zakres.Size = new System.Drawing.Size(22, 33);
+            this.zatwierz_zakres.TabIndex = 17;
+            this.zatwierz_zakres.Text = "👌";
+            this.zatwierz_zakres.UseVisualStyleBackColor = true;
+            this.zatwierz_zakres.Click += new System.EventHandler(this.zatwierz_zakres_Click);
+            // 
+            // zakres_od
+            // 
+            this.zakres_od.Font = new System.Drawing.Font("MV Boli", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.zakres_od.Location = new System.Drawing.Point(44, 67);
+            this.zakres_od.Margin = new System.Windows.Forms.Padding(2);
+            this.zakres_od.Maximum = new decimal(new int[] {
+            999,
+            0,
+            0,
+            0});
+            this.zakres_od.Name = "zakres_od";
+            this.zakres_od.Size = new System.Drawing.Size(53, 33);
+            this.zakres_od.TabIndex = 19;
+            // 
+            // zakres_do
+            // 
+            this.zakres_do.Font = new System.Drawing.Font("MV Boli", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.zakres_do.Location = new System.Drawing.Point(137, 67);
+            this.zakres_do.Margin = new System.Windows.Forms.Padding(2);
+            this.zakres_do.Maximum = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            this.zakres_do.Name = "zakres_do";
+            this.zakres_do.Size = new System.Drawing.Size(57, 33);
+            this.zakres_do.TabIndex = 20;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(231, 394);
+            this.Controls.Add(this.zakres_do);
+            this.Controls.Add(this.zakres_od);
+            this.Controls.Add(this.zatwierz_zakres);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.button11);
+            this.Controls.Add(this.button12);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.Ekran);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "ZGADYWANKA";
+            ((System.ComponentModel.ISupportInitialize)(this.zakres_od)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.zakres_do)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private TextBox Ekran;
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private Button button5;
+        private Button button6;
+        private Button button7;
+        private Button button8;
+        private Button button9;
+        private Button button10;
+        private Button button11;
+        private Button button12;
+        private Label label1;
+        private Label label2;
+        private Button zatwierz_zakres;
+        private NumericUpDown zakres_od;
+        private NumericUpDown zakres_do;
+    }
+}
---
+++ Zgadywanka/Zgadywanka/Form1.cs
@@ -0,0 +1,186 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+
+namespace Zgadywanka
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Wyzeruj()
+        {
+            if (Ekran.Text.StartsWith("Zgaduj"))
+            {
+                Ekran.Text = "";
+            }
+            if (Ekran.Text.StartsWith("Za duza"))
+            {
+                Ekran.Text = "";
+            }
+            if (Ekran.Text.StartsWith("Za mala"))
+            {
+                Ekran.Text = "";
+            }
+            if (Ekran.Text.StartsWith("Brawo"))
+            {
+                Ekran.Text = "";
+            }
+        }
+        int liczba_od, liczba_do;
+        int licznik = 1;
+        int wylosowana;
+        bool jest_wylosowana = false;
+        bool zakres_wybrany = false;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (jest_wylosowana && zakres_wybrany)
+            {
+                Wyzeruj();
+                Ekran.Text += "1";
+            }
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            if (jest_wylosowana && zakres_wybrany)
+            {
+                Wyzeruj();
+                Ekran.Text += "2";
+            }
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+            if (jest_wylosowana && zakres_wybrany)
+            {
+                Wyzeruj();
+                Ekran.Text += "3";
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (jest_wylosowana && zakres_wybrany)
+            {
+                Wyzeruj();
+                Ekran.Text += "4";
+            }
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            if (jest_wylosowana && zakres_wybrany)
+            {
+                Wyzeruj();
+                Ekran.Text += "5";
+            }
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            if (jest_wylosowana && zakres_wybrany)
+            {
+                Wyzeruj();
+                Ekran.Text += "6";
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (jest_wylosowana && zakres_wybrany)
+            {
+                Wyzeruj();
+                Ekran.Text += "7";
+            }
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            if (jest_wylosowana && zakres_wybrany)
+            {
+                Wyzeruj();
+                Ekran.Text += "8";
+            }
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            if (jest_wylosowana && zakres_wybrany)
+            {
+                Wyzeruj();
+                Ekran.Text += "9";
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if (jest_wylosowana && zakres_wybrany)
+            {
+                Wyzeruj();
+                Ekran.Text += "0";
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (zakres_wybrany)
+            {
+                Ekran.Text = "Zgaduj";
+                jest_wylosowana = true;
+                Random rand = new Random();
+                wylosowana = rand.Next(liczba_od, liczba_do);
+            }
+        }
+
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            if (zakres_wybrany && jest_wylosowana)
+            {
+                    int wartosc = Convert.ToInt32(Ekran.Text);
+                    if (wylosowana == wartosc)
+                    {
+                    Ekran.Text = "";
+                        Ekran.Text = "Brawo za: " + licznik + "x";
+                        licznik = 1;
+                        zakres_wybrany = false;
+                        jest_wylosowana = false;
+                    Task.Delay(5000).Wait();
+                    Ekran.Text = "↓Podaj zakres↓";
+                    }
+                    if (wylosowana > wartosc)
+                    {
+                        Ekran.Text = "Za mala";
+                        licznik++;
+                    }
+                    if (wylosowana < wartosc)
+                    {
+                        Ekran.Text = "Za duza";
+                        licznik++;
+                    }
+            }
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void zatwierz_zakres_Click(object sender, EventArgs e)
+        {
+            liczba_od = Convert.ToInt32(zakres_od.Value);
+            liczba_do = Convert.ToInt32(zakres_do.Value);
+            zakres_wybrany = true;
+            Ekran.Text = "Kliknij *";
+
+        }
+    }
+}
